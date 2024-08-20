cbe72a8befd5a29c388b6603382926c5615d1718
David McGinnis <david.mcginnis@bl-mc.se>
2024-08-20 09:15:30 +0200

    Create app18-badgeOverview-domSetup.js

---
+++ app18-badgeOverview-domSetup.js
@@ -0,0 +1,21 @@
+                this.siteCanvas = document.createElement("canvas");
+                this.cardBodyDiv.appendChild(this.siteCanvas);
+                let siteImage = new Image();
+                siteImage.src = this.config.image;
+                this.siteImage = siteImage;
+                this.badges = [];
+
+                let canvasWidth = this.cardBodyDiv.offsetWidth;
+                let canvasHeight = Math.round(this.cardBodyDiv.offsetWidth * siteImage.naturalHeight / siteImage.naturalWidth);
+                this.siteCanvas.width = canvasWidth;
+                this.siteCanvas.height = canvasHeight;
+
+                let siteContext = this.siteCanvas.getContext('2d');
+                siteImage.onload = function()
+                {
+                    siteContext.drawImage(siteImage, 0, 0, canvasWidth, canvasHeight );
+                }
+
+                this.siteCanvas.onmousedown = event => {this.onMouseDown(event)};
+                window.addEventListener("resize", event => {this.resize()});
+
